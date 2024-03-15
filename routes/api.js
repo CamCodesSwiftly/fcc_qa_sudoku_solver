fe1181255009d8fb4edb84129662437000d8a1ef
Kamran Babazadeh <99259950+CamCodesSwiftly@users.noreply.github.com>
2024-03-15 09:40:03 +0100

    feat: POST /api/check - Handle all cases that would fail
    Invalid characters in puzzle,
    Puzzle too long or too short,
    puzzle, coordinate or value missing,
    Invalid coordinate,
    Invalid value,

--- routes/api.js
+++ routes/api.js
@@ -5,27 +5,57 @@ const SudokuSolver = require("../controllers/sudoku-solver.js");
 module.exports = function (app) {
 	let solver = new SudokuSolver();
 
-	app.route("/api/check").post((req, res) => {});
+	app.route("/api/check").post((req, res) => {
+		let puzzle = req.body.puzzle;
+		let coordinate = req.body.coordinate;
+		let value = req.body.value;
+
+		// (IGNORE missing puzzle, will handle in c)
+		// a) puzzle too long/short? b) puzzle has invalid characters?
+		let puzzleValidationResult = solver.validate(puzzle);
+		if (
+			puzzleValidationResult === "Invalid characters in puzzle" ||
+			puzzleValidationResult ===
+				"Expected puzzle to be 81 characters long"
+		) {
+			return res.json({ error: puzzleValidationResult });
+		}
+
+		// c) puzzle, value or coordinate missing?
+		if (
+			coordinate === "undefined" ||
+			!coordinate ||
+			value === "undefined" ||
+			!value ||
+			puzzle === "undefined" ||
+			!puzzle
+		) {
+			return res.json({ error: "Required field(s) missing" });
+		}
+		// coordinate not correct
+		const coordinateRegex = /[A-I][1-9]/g;
+		let coordinateWrong = coordinate.match(coordinateRegex);
+		if (!coordinateWrong || coordinate.length > 2) {
+			console.log(coordinate);
+			return res.json({ error: "Invalid coordinate" });
+		}
+		// value is not 1-9
+		const oneToNineRegex = /[^1-9]/g;
+		let valueNotAllowed = value.match(oneToNineRegex);
+		if (valueNotAllowed) {
+			return res.json({ error: "Invalid value" });
+		}
+
+		if (coordinate) return res.json("temporary");
+	});
 
 	app.route("/api/solve").post((req, res) => {
 		let puzzleString = req.body.puzzle;
 
 		const validationResult = solver.validate(puzzleString);
 
-		switch (validationResult) {
-			case "Required field missing":
-				return res.json({ error: "Required field missing" });
-				break;
-			case "Invalid characters in puzzle":
-				return res.json({ error: "Invalid characters in puzzle" });
-				break;
-			case "Expected puzzle to be 81 characters long":
-				return res.json({
-					error: "Expected puzzle to be 81 characters long",
-				});
-				break;
-			default:
-				break;
+		if (validationResult !== true) {
+			return res.json({ error: validationResult });
 		}
 
 		let solution = solver.solve(puzzleString);
@@ -33,7 +63,7 @@ module.exports = function (app) {
 		// puzzle cant be solved?
 		// if the solution has a . in it, it was not solved
 		const unsolvableRegex = /\./g;
-		const unsolvable = solution.match(unsolvableRegex);
+		let unsolvable = solution.match(unsolvableRegex);
 		if (unsolvable) {
 			return res.json({ error: "Puzzle cannot be solved" });
 		}
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,11 +1,34 @@
 const chai = require("chai");
-const chaiHttp = require('chai-http');
+const chaiHttp = require("chai-http");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', () => {
+suite("Functional Tests", () => {
+	test("POST /api/check - No puzzle given", function (done) {
+		chai.request(server)
+			.post("/api/check")
+			.send({
+				coordinate: "A1",
+				value: "8",
+			}) // Provide the desired query parameters
+			.end(function (err, res) {
+				assert.equal(err, null); // No error should occur
+				assert.equal(res.status, 200);
+				assert.property(
+					res.body,
+					"error",
+					"error property should exist in response"
+				);
+				// Check for correct error message
+				assert.equal(
+					res.body.error,
+					"Required field(s) missing",
+					"Error should equal Required field(s) missing"
+				);
 
+				done();
+			});
+	});
 });
-
